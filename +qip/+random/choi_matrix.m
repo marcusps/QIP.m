679d80098a2cda75cf2bb60833ab097846c732ed
Marcus P S <marcus.ps+github@gmail.com>
2010-06-01 18:52:15 -0400

    Added copyright notices to all files in qip.random.*

--- +qip/+random/choi_matrix.m
+++ +qip/+random/choi_matrix.m
@@ -15,9 +15,26 @@ function phi = choi_matrix( d )
 % 
 %   See also: qip.random.density_matrix, qip.random.unitary
 %
-%    License: Distributed under GPL 2.0
-%             http://creativecommons.org/licenses/GPL/2.0/
-%             http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%   Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 %
+
+%  Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 phi = qip.random.mixed_state(d*d);
--- +qip/+random/mixed_state.m
+++ +qip/+random/mixed_state.m
@@ -14,10 +14,27 @@ function rho = mixed_state(d)
 % 
 %   See also: qip.random.unitary
 %
-%    License: Distributed under GPL 2.0
-%             http://creativecommons.org/licenses/GPL/2.0/
-%             http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%   Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 %
+
+%  Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 u = qip.random.unitary(d);
 e = rand(1,d);
