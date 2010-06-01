d5c375ae79549a012fa5cdc6c89a42299c631fa3
Marcus P S <marcus.ps+github@gmail.com>
2010-06-01 18:54:28 -0400

    Added copyright notices to all files in qip.open_systems.*

--- +qip/+open_systems/real2complex_mat.m
+++ +qip/+open_systems/real2complex_mat.m
@@ -11,9 +11,26 @@ function m = real2complex_mat( in )
 %
 %    See also: vec, row, rowinv, reshape
 %
-%    License: Distributed under GPL 2.0
-%             http://creativecommons.org/licenses/GPL/2.0/
-%             http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%   Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 %
+
+%  Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 m = kron(real(in),eye(2)) + kron(imag(in),[0 -1; 1 0]);
