f8d44a9d473afe2abc7257f77152ee215161d509
Marcus P S <marcus.ps+github@gmail.com>
2010-06-01 22:52:13 -0400

    Added copyright notices to hamiltonian and vec functions.

--- +qip/+open_systems/hamiltonian.m
+++ +qip/+open_systems/hamiltonian.m
@@ -1,3 +1,34 @@
 function L = hamiltonian( h )
 
+% QIP.OPEN_SYSTEMS.HAMILTONIAN  Liouvillian representation of hamiltonian superoperator
+% requires: nothing
+% author: Marcus da Silva
+%
+%    L = qip.open_systems.hamiltonian(A) returns a column-major
+%    liouville representation of a hamiltonian operator A.
+%
+%    See also: liou_row, vec, row
+%
+%   Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%
+
+%  Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 L = -1j * liou(h,eye(size(h))) + 1j * liou(eye(size(h)),h);
--- +qip/+open_systems/vec.m
+++ +qip/+open_systems/vec.m
@@ -1,3 +1,34 @@
 function V = vec( M )
 
+% QIP.OPEN_SYSTEMS.VEC  Column-major vectorization
+% requires: nothing
+% author: Marcus da Silva
+%
+%    L = qip.open_systems.vec(A) returns a column-major vectorization
+%    (i.e. stacking of columns) of a matrix A.
+%
+%    See also: liou_row, vec, row
+%
+%   Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%
+
+%  Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
   V = reshape(M,prod(size(M)),1);
