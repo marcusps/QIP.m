a1578854012a5fa6f9684fcc1e4880fdb049dfa5
Marcus P S <marcus.ps+github@gmail.com>
2010-11-24 15:41:54 -0500

    Added utility functions to normalize vectors and to build projectors out of vectors.

---
+++ +qip/normalize.m
@@ -0,0 +1,29 @@
+function vn = normalize( v )
+% NORMALIZE
+% author: Marcus P. da Silva
+% requires: none
+%
+%   NORMALIZE(v) returns a unit vector along the direction of v.
+%
+%   Copyright (C) 2010  Marcus P. da Silva  http://github.com/marcusps
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%
+
+%  Copyright (C) 2010  Marcus P. da Silva  http://github.com/marcusps
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
+  vn = v/norm(v);
---
+++ +qip/projector.m
@@ -0,0 +1,31 @@
+function p = projector( v )
+% PROJECTOR
+% author: Marcus P. da Silva
+% requires: none
+%
+%   PROJECTOR(v) returns the projector corresponding to the
+%   vector v.
+%
+%   Copyright (C) 2010  Marcus P. da Silva  http://github.com/marcusps
+% 
+%   License: Distributed under GPL 2.0
+%            http://creativecommons.org/licenses/GPL/2.0/
+%            http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+%
+
+%  Copyright (C) 2010  Marcus P. da Silva  http://github.com/marcusps
+% 
+%  This program is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+p = v*v';
