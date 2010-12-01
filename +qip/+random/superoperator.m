a1095ec4bd8dbcea49da0d1fd713d08de4444ff1
Marcus P S <marcus.ps+github@gmail.com>
2010-12-01 12:43:24 -0500

    Added function to generate random superoperators

--- +qip/+random/superoperator.m
+++ +qip/+random/superoperator.m
@@ -1,5 +1,19 @@
-function phis = dynamical_matrix(d)
-
+function phis = superoperator(d)
+% QIP.RANDOM.SUPEROPERATOR  Liouvillian representation of Kraus operators
+% requires: qip.open_systems.choi2liou, qip.random.choi_matrix
+% author: Marcus da Silva
+%
+%    L = qip.random.superoperator(d) returns the column-major
+%    liouville representation of a random superoperator acting on a d
+%    dimensional Hilbert space. The superoperator is chosen by
+%    generating a random Choi matrix, and then shuffling the matrix
+%    elements accordingly.
+%
+%    IMPORTANT: There is no guarantee that the generated superoperator
+%    is trace preserving.
+%
+%    See also: choi2liou, choi_matrix
+%
 %   Copyright (C) 2010   Marcus P da Silva http://github.com/marcusps
 % 
 %   License: Distributed under GPL 2.0
@@ -21,5 +35,4 @@ function phis = dynamical_matrix(d)
 % 
 %  You should have received a copy of the GNU General Public License
 %  along with this program; if not, see <http://www.gnu.org/licenses/>.
-
-phis = qip.open_systems.choi_shuffle(qip.random.rand_choi_matrix(d));
+phis = qip.open_systems.choi2liou(qip.random.choi_matrix(d));
